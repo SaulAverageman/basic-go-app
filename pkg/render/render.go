d5a79eb1623d6f6f337854a12afdd49df9beda8b
sleepwalker <bubai.official9119@gmail.com>
2023-02-13 01:39:16 +0530

    `date`

---
+++ pkg/render/render.go
@@ -0,0 +1,18 @@
+package renders
+
+import (
+	"fmt"
+	"net/http"
+	"text/template"
+)
+
+// renders and writes templats based on given tmpl name
+func RenderTemplate(w http.ResponseWriter, tmpl string) {
+	parsedTemplate, _ := template.ParseFiles("./templates/", tmpl)
+	err := parsedTemplate.Execute(w, nil)
+
+	if err != nil {
+		fmt.Println("error rendering template: ", tmpl, "with error:", err)
+		return
+	}
+}
