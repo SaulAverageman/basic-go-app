e9d7553a787506c7204a43f740682b2f35d6c7ea
sleepwalker <bubai.official9119@gmail.com>
2023-03-12 21:15:06 +0530

    create template cache

--- Dockerfile
+++ Dockerfile
@@ -2,6 +2,6 @@ FROM golang:1.19-alpine
 RUN adduser -S goruntime
 USER goruntime
 WORKDIR /work
-COPY ./src/ /work/
+COPY ./src /work/
 CMD ["go", "run", "/work/cmd/web/main.go"]
 EXPOSE 8000
--- src/pkg/render/render.go
+++ src/pkg/render/render.go
@@ -3,16 +3,61 @@ package render
 import (
 	"fmt"
 	"net/http"
+	"os"
 	"text/template"
 )
 
 // renders and writes templats based on given tmpl name
 func RenderTemplate(w http.ResponseWriter, tmpl string) {
-	parsedTemplate, _ := template.ParseFiles("./templates/"+tmpl, "./templates/base.layout.tmpl")
-	err := parsedTemplate.Execute(w, nil)
+	//root project path
+	WORKPATH := "/work/"
 
+	//caching the templates in templateCache map
+	templateCache, err := formTemplateCache(WORKPATH)
+
+	if err != nil {
+		fmt.Println("error: template cache is not available, loading from disk")
+		//loading the specific file into cache as the file is not found in the cache
+		templateCache[tmpl], err = template.ParseFiles(WORKPATH+"templates/"+tmpl, WORKPATH+"templates/base.layout.tmpl")
+
+		if err != nil {
+			fmt.Println("error: loading", tmpl, "beacuse", err)
+			return
+		}
+	}
+
+	//loading the file from cache
+	parsedTemplate, fileExists := templateCache[tmpl]
+	if !fileExists {
+		fmt.Println("error: template file doesnot exsit", tmpl)
+		return
+	}
+
+	//writing the file in http response writer
+	err = parsedTemplate.Execute(w, nil)
 	if err != nil {
 		fmt.Println("error rendering template: ", tmpl, "with error:", err)
 		return
 	}
+}
+
+func formTemplateCache(WORKPATH string) (map[string]*template.Template, error) {
+	fmt.Println("verbose: loading template cache")
+
+	tc := map[string]*template.Template{}
+
+	files, err := os.ReadDir(WORKPATH + "templates/")
+	if err != nil {
+		fmt.Println("error: cannot read files at WORKPATH,", err)
+		return nil, err
+	}
+
+	for _, file := range files {
+		tc[file.Name()], err = template.ParseFiles(WORKPATH+"templates/"+file.Name(), WORKPATH+"templates/base.layout.tmpl")
+		if err != nil {
+			fmt.Println("error: cannot read file", WORKPATH+"templates/"+file.Name())
+			return nil, err
+		}
+	}
+	return tc, nil
 }
