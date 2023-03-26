9656ab4d31192e5426cd6dc71b3a6ed8cc0d7e8c
WSL user <bubaimondal9119@gmail.com>
2023-03-27 00:37:44 +0530

    setup app wide config

--- src/cmd/web/main.go
+++ src/cmd/web/main.go
@@ -1,12 +1,29 @@
 package main
 
 import (
+	"log"
 	"net/http"
 
+	"github.com/saulaverageman/basic-go-app/pkg/config"
 	"github.com/saulaverageman/basic-go-app/pkg/handler"
+	"github.com/saulaverageman/basic-go-app/pkg/render"
 )
 
 func main() {
+	// app is the app config for the whole project
+	var app config.AppConfig
+
+	// render template cache as soon as the app starts and saving them in appConfig.TemplateCache
+	tc, err := render.FormTemplateCache("/work/")
+	if err != nil {
+		log.Fatal("error: cannot create template cache")
+	} else {
+		app.TemplateCache = tc
+	}
+
+	// sending app config to render.go
+	render.SetRenderAppConfig(&app)
+
 	const appPort = ":8000"
 	http.HandleFunc("/", handler.Home)
 	http.HandleFunc("/about", handler.About)
--- src/pkg/render/render.go
+++ src/pkg/render/render.go
@@ -1,61 +1,67 @@
 package render
 
 import (
-	"fmt"
+	"log"
 	"net/http"
 	"os"
 	"text/template"
+
+	"github.com/saulaverageman/basic-go-app/pkg/config"
 )
+
+var appConfig *config.AppConfig
+var err error
+
+func SetRenderAppConfig(config *config.AppConfig) {
+	appConfig = config
+}
 
 // renders and writes templats based on given tmpl name
 func RenderTemplate(w http.ResponseWriter, tmpl string) {
-	//root project path
 	WORKPATH := "/work/"
 
-	//caching the templates in templateCache map
-	templateCache, err := formTemplateCache(WORKPATH)
+	// getting the templateCache from the cental app config
+	templateCache := appConfig.TemplateCache
 
-	if err != nil {
-		fmt.Println("error: template cache is not available, loading from disk")
-		//loading the specific file into cache as the file is not found in the cache
+	// loading the file from cache
+	parsedTemplate, fileExists := templateCache[tmpl]
+	if !fileExists {
+		log.Panic("error: template cache is not available, loading from disk")
+
+		// loading the specific file into cache as the file is not found in the cache
 		templateCache[tmpl], err = template.ParseFiles(WORKPATH+"templates/"+tmpl, WORKPATH+"templates/base.layout.tmpl")
 
 		if err != nil {
-			fmt.Println("error: loading", tmpl, "beacuse", err)
+			log.Fatal("error: loading", tmpl, "beacuse", err)
 			return
 		}
 	}
 
-	//loading the file from cache
-	parsedTemplate, fileExists := templateCache[tmpl]
-	if !fileExists {
-		fmt.Println("error: template file doesnot exsit", tmpl)
-		return
-	}
-
-	//writing the file in http response writer
+	// writing the file in http response writer
+	log.Println("verbose: serving from cache template-", tmpl)
 	err = parsedTemplate.Execute(w, nil)
 	if err != nil {
-		fmt.Println("error rendering template: ", tmpl, "with error:", err)
+		log.Fatal("error rendering template: ", tmpl, "with error:", err)
 		return
 	}
 }
 
-func formTemplateCache(WORKPATH string) (map[string]*template.Template, error) {
-	fmt.Println("verbose: loading template cache")
+// func to create a cache with all the templates
+func FormTemplateCache(WORKPATH string) (map[string]*template.Template, error) {
+	log.Print("verbose: loading template cache")
 
 	tc := map[string]*template.Template{}
 
 	files, err := os.ReadDir(WORKPATH + "templates/")
 	if err != nil {
-		fmt.Println("error: cannot read files at WORKPATH,", err)
+		log.Fatal("error: cannot read files at WORKPATH,", err)
 		return nil, err
 	}
 
 	for _, file := range files {
 		tc[file.Name()], err = template.ParseFiles(WORKPATH+"templates/"+file.Name(), WORKPATH+"templates/base.layout.tmpl")
 		if err != nil {
-			fmt.Println("error: cannot read file", WORKPATH+"templates/"+file.Name())
+			log.Panic("error: cannot read file", WORKPATH+"templates/"+file.Name())
 			return nil, err
 		}
 	}
