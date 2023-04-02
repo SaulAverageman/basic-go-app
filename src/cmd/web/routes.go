80d9851266e45635f5437c09c94ea4ef82fa786f
WSL user <bubaimondal9119@gmail.com>
2023-04-03 02:09:58 +0530

    replace pat router with chi"

--- src/cmd/web/routes.go
+++ src/cmd/web/routes.go
@@ -3,7 +3,7 @@ package main
 import (
 	"net/http"
 
-	"github.com/bmizerany/pat"
+	"github.com/go-chi/chi"
 	"github.com/saulaverageman/basic-go-app/pkg/config"
 	"github.com/saulaverageman/basic-go-app/pkg/handler"
 )
@@ -12,9 +12,13 @@ var appConfig *config.AppConfig
 
 func routes(app *config.AppConfig) http.Handler {
 	appConfig = app
-	mux := pat.New()
-	mux.Get("/", http.HandlerFunc(handler.Home))
-	mux.Get("/about", http.HandlerFunc(handler.About))
+	// mux := pat.New()
+	// mux.Get("/", http.HandlerFunc(handler.Home))
+	// mux.Get("/about", http.HandlerFunc(handler.About))
+
+	mux := chi.NewMux()
+	mux.Get("/", handler.Home)
+	mux.Get("/about", handler.About)
 
 	return mux
 }
